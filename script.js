3a78834b86a743168cdd9483f4e750caf8074b93
sunilsuraci <suracisunil@gmail.com>
2023-04-07 12:07:19 +0200

    add class

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+class Calculator {
+    constructor(previusOperandTextElement, currentOperandTextElement) {
+        this.previusOperandTextElement = previusOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
+
+
+
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
